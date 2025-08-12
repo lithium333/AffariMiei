87fe7ebbb83be742e07f684d30fb461a3515d993
lithium333 <119898873+lithium333@users.noreply.github.com>
2025-08-12 14:24:29 +0200

    2025 edition - Client on single file

--- client/regia.py
+++ client/regia.py
@@ -58,25 +58,36 @@ def modereg():
 	requests.get("http://"+ip_addr+"/modereg.php")
 	window.destroy()
 	drwFrame()
-def modepac():
-	try:
-		fobj = open("partite.txt")
-	except:
-		fobj = []
-	
+
+def azzera():
+	#try:
+	#	fobj = open("partite.txt")
+	#except:
+	#	fobj = []
+	#
 	requests.get("http://"+ip_addr+"/azzera.php")
-	for riga in fobj:
-		riga = riga.rstrip()
-		rigav = riga.split(",")
-		col = int(rigav[0])
-		row = int(rigav[1])
-		requests.get("http://"+ip_addr+"/write.php?col="+str(col)+"&row="+str(row))
+	#for riga in fobj:
+	#	riga = riga.rstrip()
+	#	rigav = riga.split(",")
+	#	col = int(rigav[0])
+	#	row = int(rigav[1])
+	#	requests.get("http://"+ip_addr+"/write.php?col="+str(col)+"&row="+str(row))
+	window.destroy()
+	drwFrame()
+
+def splashON():
+	requests.get("http://"+ip_addr+"/splashon.php")
+	window.destroy()
+	drwFrame()
+
+def splashOFF():
+	requests.get("http://"+ip_addr+"/splashoff.php")
 	window.destroy()
 	drwFrame()
 
 def drwFrame():
 	global window
-	window = tk.Frame(windowParent,height = 900, width = 1000)
+	window = tk.Frame(windowParent,height = 700, width = 1000)
 	
 	# JSON LOAD
 	jrqst = requests.get("http://"+ip_addr+"/data/pacchi.json")
@@ -100,7 +111,7 @@ def drwFrame():
 	last_valido=[0,0]
 
 	if(jdata[7]):
-		for cnt in range(0,15):
+		for cnt in range(0,10):
 			exec("arr_cbox_b.append(tk.Button(master=window, bg='yellow', fg='black', text=jdata[5][cnt][\"desc\"], command=lambda: toglireg(0,"+str(cnt)+")))")
 			arr_cbox_b[cnt].place(x=100,y=100+cnt*40)
 			if(not jdata[5][cnt]["show"]):
@@ -111,7 +122,7 @@ def drwFrame():
 				last_valido[0]=0
 				last_valido[1]=cnt
 			print(str(cnt)+" : "+jdata[0][cnt]["desc"])
-		for cnt in range(0,15):
+		for cnt in range(0,10):
 			exec("arr_cbox_r.append(tk.Button(master=window, bg='yellow', fg='black', text=jdata[6][cnt][\"desc\"], command=lambda: toglireg(1,"+str(cnt)+")))")
 			arr_cbox_r[cnt].place(x=400,y=100+cnt*40)
 			if(not jdata[6][cnt]["show"]):
@@ -124,7 +135,7 @@ def drwFrame():
 			print(str(cnt)+" : "+jdata[1][cnt]["desc"])
 	else:
 		print("\nBLU:")
-		for cnt in range(0,15):
+		for cnt in range(0,10):
 			exec("arr_cbox_b.append(tk.Button(master=window, bg='blue', fg='white', text=jdata[0][cnt][\"desc\"], command=lambda: apripacco(0,"+str(cnt)+")))")
 			arr_cbox_b[cnt].place(x=100,y=100+cnt*40)
 			if(not jdata[0][cnt]["show"]):
@@ -136,7 +147,7 @@ def drwFrame():
 				last_valido[1]=cnt
 			print(str(cnt)+" : "+jdata[0][cnt]["desc"])
 		print("\nROSSI:")
-		for cnt in range(0,15):
+		for cnt in range(0,10):
 			exec("arr_cbox_r.append(tk.Button(master=window, bg='red', fg='white', text=jdata[1][cnt][\"desc\"], command=lambda: apripacco(1,"+str(cnt)+")))")
 			arr_cbox_r[cnt].place(x=400,y=100+cnt*40)
 			if(not jdata[1][cnt]["show"]):
@@ -151,20 +162,20 @@ def drwFrame():
 		# OFFERTA
 		if(jdata[3]!=None):
 			butt_del = tk.Button(master=window, bg='orange', text="RIMUOVI OFFERTA", command=lambda: delofferta())
-			butt_del.place(x=100,y=750)
+			butt_del.place(x=100,y=550)
 			if(not jdata[4]):
 				butt_acc = tk.Button(master=window, bg='lime', text="ACCETTA OFFERTA", command=lambda: accofferta())
-				butt_acc.place(x=400,y=750)
+				butt_acc.place(x=400,y=550)
 			desc_offer = tk.Button(master=window, bg='yellow', text="OFFERTA: "+jdata[3], state='disabled')
 			if(jdata[4]):
 				desc_offer.config(bg="lime")
-			desc_offer.place(x=700,y=750)
+			desc_offer.place(x=700,y=550)
 		else:
 			butt_del = tk.Button(master=window, bg='yellow', text="FAI OFFERTA", command=lambda: faiofferta())
-			butt_del.place(x=100,y=750)
+			butt_del.place(x=100,y=550)
 			global ent_offer
 			ent_offer = tk.Entry(window, width=20)
-			ent_offer.place(x=400,y=750)
+			ent_offer.place(x=400,y=550)
 	
 	# LOAD BOTTONI SUONI
 	acfg_file = open("sound.cfg")
@@ -186,20 +197,28 @@ def drwFrame():
 	
 	# CONTRADA FORTUNATA
 	if(jdata[7]):
-		butt_contrada = tk.Button(master=window, bg='black', fg='white', text="AZZERA (MOD. PACCHI)", command=lambda: modepac())
-		butt_contrada.place(x=100,y=840)
+		butt_contrada = tk.Button(master=window, bg='#CF5F00', fg='white', text="AZZERA (MOD. PACCHI)", command=lambda: azzera())
+		butt_contrada.place(x=100,y=640)
+		if(jdata[8]):
+			butt_contrada2 = tk.Button(master=window, bg='black', fg='white', text="SPLASH OFF REGIONE", command=lambda: splashOFF())
+			butt_contrada2.place(x=400,y=640)
+		else:
+			butt_contrada2 = tk.Button(master=window, bg='black', fg='white', text="SPLASH ON REGIONE", command=lambda: splashON())
+			butt_contrada2.place(x=400,y=640)
 	else:
-		butt_contrada = tk.Button(master=window, bg='black', fg='white', text="CONTRADA FORTUNATA", command=lambda: modereg())
-		butt_contrada.place(x=100,y=840)
+		butt_contrada = tk.Button(master=window, bg='#CF5F00', fg='white', text="AZZERA", command=lambda: azzera())
+		butt_contrada.place(x=100,y=640)
+		butt_contrada2 = tk.Button(master=window, bg='black', fg='white', text="CONTRADA FORTUNATA", command=lambda: modereg())
+		butt_contrada2.place(x=400,y=640)
 		
-	# EXIT: partita finita
-	if(totpacchi<2):
-		window.destroy();
-		messagebox.showinfo("PARTITA TERMINATA", "Il pacco mancante sta per essere memorizzato sul file.")
-		fobj = open("partite.txt","a")
-		fobj.write(str(last_valido[0])+","+str(last_valido[1])+"\n")
-		fobj.close()
-		exit()
+	# EXIT: partita finita (DISABLED)
+	#if(totpacchi<2):
+	#	window.destroy();
+	#	messagebox.showinfo("PARTITA TERMINATA", "Il pacco mancante sta per essere memorizzato sul file.")
+	#	fobj = open("partite.txt","a")
+	#	fobj.write(str(last_valido[0])+","+str(last_valido[1])+"\n")
+	#	fobj.close()
+	#	exit()
 	window.pack()
 	window.pack_propagate(0)
 	
@@ -208,7 +227,7 @@ def drwFrame():
 windowParent = tk.Tk()
 windowParent.call('wm', 'attributes', '.', '-topmost', '1')
 windowParent.title("Affari Miei MGR")
-windowParent.geometry('1000x900') 
+windowParent.geometry('1000x700') 
 windowParent.protocol('WM_DELETE_WINDOW', lambda: exit())
 
 while(True):
@@ -226,24 +245,3 @@ while(True):
 	
 		
 	windowParent.mainloop()
-
-		
-
-
-# DIMSESSO
-#try:
-#	c1 = int(input("\ncolonna: "))
-#	if(c1==-1):
-#		running=False
-#except:
-#	c1=-2
-#try:
-#	c2 = int(input("riga: "))
-#	if(c2==-1):
-#		running=False
-#except:
-#	c2=-2
-
-
-#if(c1>=0 and c2>=0 and c1<2 and c2<15 and running):
-#	requests.get("http://"+ip_addr+"/write.php?col="+str(c1)+"&row="+str(c2))
